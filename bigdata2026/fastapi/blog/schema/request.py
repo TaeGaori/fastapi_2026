b2067a7d6507516edba8b1103bb5cd7f6801fccf
TaeGaori <xoengur@gmail.com>
2026-06-18 17:19:06 +0900

    6월 18일

--- bigdata2026/fastapi/blog/schema/request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class PostCreateRequest(BaseModel):
-    title: str
-    content: str
-
-class PostUpdateRequest(BaseModel):
-    title: str  |None = None
-    content: str |None = None
--- bigdata2026/fastapi/blog/schema/response.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Postresponse(BaseModel):
-    id : int
-    title : str
-    content : str
