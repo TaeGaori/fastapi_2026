84d5a5c0c9d3c25702fe46f2a3582ae3249f0218
TaeGaori <xoengur@gmail.com>
2026-08-26 17:46:00 +0900

    8월 26일

---
+++ penguin-project/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, status, HTTPException
+from train import train
+import pickle
+import joblib
+from pydantic import BaseModel
+
+class Featureinput(BaseModel)
+
+app = FastAPI(
+    title='펭귄 예측'
+)
+
+
+# 모델링으로 만든 pkl파일 불러오기
+MODEL_PATH = 'penguin_model.pkl'
+try:
+    model = joblib.load(MODEL_PATH)
+    print('로드 성공')
+except Exception as e:
+    print('로드 실패 ')
+    model = None
+
+@app.get('/',
+         status_code=status.HTTP_200_OK
+)
+def root():
+    return{
+        'message': '서버 실행'
+    }   
+
+
+@app.get('/model-info/')
+def input_model(
+
+)
