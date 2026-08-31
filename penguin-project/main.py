13dea0247f09b061d22edfda1b9d3a2f8a793a6c
TaeGaori <xoengur@gmail.com>
2026-08-31 09:21:18 +0900

    8월 31일

--- penguin-project/main.py
+++ penguin-project/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException, status
-import pickle
 import joblib
 from pydantic import BaseModel
 import numpy as np
+import os
 
 class Featureinput(BaseModel):
     bill_length_mm:float
@@ -18,9 +18,10 @@ app = FastAPI(
 
 # 모델링으로 만든 pkl파일 불러오기
 # pkl를 어떻게 적용하는지 질문
-MODEL_PATH = 'penguin_model.pkl'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(BASE_DIR, "penguin_model.pkl")
 try:
-    model = joblib.load(MODEL_PATH)
+    model = joblib.load(PATH)
     print('로드 성공')
 except Exception as e:
     print('로드 실패 ')
@@ -40,7 +41,7 @@ def input_model_size(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass
             }
 
 
-
+# 이 부분은 거의 다 물어보고 했어요
 # features에 input_model_size -> Featureinput으로 변경
 @app.post('/predict/')
 def predict(features: Featureinput):
@@ -48,7 +49,7 @@ def predict(features: Featureinput):
         # 모델 로드 안 되었을 때 HTTP오류 코드 몇 번인지 질문 -> 500 작성
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detali='모델이 로드되지 않았습니다.'
+            detail='모델이 로드되지 않았습니다.'
         )
 
     # np.array() -> numpy 배열로 바꿔줌
@@ -63,5 +64,7 @@ def predict(features: Featureinput):
 
     return{
         "입력": features,
-        '예측': prediction[0]
+        # model.predict(X)가 numpy 배열을 반환 -> prediction[0]도 numpy형태로 반환 가능성 높음 ->
+        # FastAPI가 Json을 직렬화 하다 에러 나는 경우 발생 -> str로 형변환
+        "예측": str(prediction[0])
     }
